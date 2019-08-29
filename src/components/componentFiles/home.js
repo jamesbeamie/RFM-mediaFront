425ff792d0a6a14a546e58a99eeb1fdad150b9c7
jamesbeamie <wafulajames9@gmail.com>
2019-08-29 17:58:32 +0300

    Create homepage

--- src/components/componentFiles/home.js
+++ src/components/componentFiles/home.js
@@ -3,32 +3,54 @@ import Carousel from 'react-bootstrap/Carousel';
 import '../../assets/styles/home.css';
 // import { Link } from 'react-router-dom';
 
-const HomePage = () => (
-	<div className="wrapper">
-		<Carousel>
-			<Carousel.Item>
-				<img src="src/assets/images/IMG_2358-1083.jpg" className="carousel_image" alt="..." />
-				<Carousel.Caption>
-					<h3>slide label 1</h3>
-					<p>slide.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img src="src/assets/images/IMG_2385-1083.jpg" className="carousel_image" alt="..." />
-				<Carousel.Caption>
-					<h3>slide label 2</h3>
-					<p>slide.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img src="src/assets/images/RFM_YO_14.jpg" className="carousel_image" alt="..." />
-				<Carousel.Caption>
-					<h3>slide label 3</h3>
-					<p>slide.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-		</Carousel>
-	</div>
-);
+const HomePage = () => {
+	return (
+		<React.Fragment>
+			<section>
+				<div className="wrapper">
+					<Carousel>
+						<Carousel.Item>
+							<img src="src/assets/images/IMG_2358-1083.jpg" className="carousel_image" alt="..." />
+							<Carousel.Caption>
+								<h3>slide label 1</h3>
+								<p>slide.</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img src="src/assets/images/IMG_2385-1083.jpg" className="carousel_image" alt="..." />
+							<Carousel.Caption>
+								<h3>slide label 2</h3>
+								<p>slide.</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img src="src/assets/images/RFM_YO_14.jpg" className="carousel_image" alt="..." />
+							<Carousel.Caption>
+								<h3>slide label 3</h3>
+								<p>slide.</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+					</Carousel>
+				</div>
+			</section>
+			<div className="container">
+				<div className="row jumbotron">
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+				</div>
+
+				<div className="row jumbotron">
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+				</div>
+				<div className="row jumbotron">
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+					<div className="col-sm-6 text-center">kdbfjnvmncsjdnjsjdjkj</div>
+				</div>
+			</div>
+			<section />
+		</React.Fragment>
+	);
+};
 
 export default HomePage;
