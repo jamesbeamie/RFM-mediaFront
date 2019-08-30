096924358f80d52e8f32a671ff403ae6914ead58
jamesbeamie <wafulajames9@gmail.com>
2019-08-30 17:50:33 +0300

    Authentication forms

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './componentFiles/home';
 import About from './componentFiles/about';
 import Header from './common/header';
+import AuthPage from './componentFiles/signUp';
+import LoginPage from './componentFiles/login';
 // import Footer from './common/footer';
 import PageNotFound from './common/PageNotFound';
 
@@ -13,6 +15,8 @@ const App = () => {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/about" component={About} />
+				<Route path="/signup" component={AuthPage} />
+				<Route path="/sigin" component={LoginPage} />
 				<Route component={PageNotFound} />
 			</Switch>
 		</div>
