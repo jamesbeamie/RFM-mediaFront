cd5a07cd5e6d82e8552928d984ac1bb08e0d8893
jamesbeamie <wafulajames9@gmail.com>
2019-09-14 11:59:27 +0300

    nothing important

--- src/components/actions/signIn.js
+++ src/components/actions/signIn.js
@@ -2,33 +2,36 @@ import { SIGNIN_SUCCESS, SIGNIN_FAILED } from './types';
 import urlPath from '../common/axiosConfig';
 
 const signInAction = (signInData) => (dispatch) => {
-	return urlPath
-		.request({
-			method: 'post',
-			headers: {
-				'content-type': 'application/json'
-			},
-			url: '/users/login/',
-			data: {
-				user: {
-					// username: signUpData.username,
-					email: signInData.email,
-					password: signInData.password
+	return (
+		urlPath
+			.request({
+				method: 'post',
+				headers: {
+					'content-type': 'application/json'
+				},
+				url: '/users/login/',
+				data: {
+					user: {
+						// username: signUpData.username,
+						email: signInData.email,
+						password: signInData.password
+					}
 				}
-			}
-		})
-		// .then((res) => res.json())
-		.then((logindata) =>
-			dispatch({
-				type: SIGNIN_SUCCESS,
-				payload: logindata
-			}, window.localStorage.setItem('token', ''))
-		)
-		.catch((err) =>
-			dispatch({
-				type: SIGNIN_FAILED,
-				payload: err
 			})
-		);
+			// .then((res) => res.json())
+			.then((logindata) =>
+				dispatch({
+					type: SIGNIN_SUCCESS,
+					payload: logindata
+				}, 
+				)
+			)
+			.catch((err) =>
+				dispatch({
+					type: SIGNIN_FAILED,
+					payload: err
+				})
+			)
+	);
 };
 export default signInAction;
